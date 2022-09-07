fb84f6db97ebb8da58299a355e341ba4a4468ab1
Maciej Stanek <eszpag@gmail.com>
2022-09-07 11:31:31 +0200

    Added the stop tests, they prove the problem with the recurring timer implementation (#1).

--- MyTimerTest/test.cpp
+++ MyTimerTest/test.cpp
@@ -8,8 +8,8 @@ using namespace std::chrono_literals;
 using std::this_thread::sleep_for;
 
 TEST(Timer, OneShotPrecision) {
-	constexpr auto runTime = 3ms;
-	constexpr auto precision = 1ms;
+	constexpr auto runTime = 10ms;
+	constexpr auto precision = 2ms;
 	auto value = false;
 	const auto setValue = [&value]() { value = true; };
 	Timer timer;
@@ -20,11 +20,23 @@ TEST(Timer, OneShotPrecision) {
 	EXPECT_TRUE(value);
 }
 
+TEST(Timer, OneShotStop) {
+	constexpr auto runTime = 10ms;
+	constexpr auto precision = 2ms;
+	auto done = false;
+	const auto action = [&done]() { done = true; };
+	Timer timer;
+	timer.callWithDelay(action, runTime);
+	sleep_for(runTime - precision);
+	timer.stop();
+	sleep_for(2 * precision);
+	EXPECT_FALSE(done);
+}
+
 TEST(Timer, PeriodicalPrecision) {
 	constexpr auto oneTick = 200ms;
-
-	// TODO: The timer precision is impacted significantly by the
-	//       run time of the callback, leading to increased period.
+	// TODO #1: The timer precision is impacted significantly by the
+	// run time of the callback, leading to increased period.
 	constexpr auto expectedCallCount = 10;
 	constexpr auto fullTime = (expectedCallCount + 0.5) * oneTick;
 	auto counter = 0;
@@ -35,6 +47,21 @@ TEST(Timer, PeriodicalPrecision) {
 	EXPECT_EQ(counter, expectedCallCount);
 }
 
-// TODO: test stop for one shot and for periodical
+TEST(Timer, PeriodicalStop) {
+	constexpr auto oneTick = 200ms;
+	// TODO #1: The timer precision is impacted significantly by the
+	// run time of the callback, leading to increased period.
+	constexpr auto expectedCallCount = 10;
+	constexpr auto fullTime = (expectedCallCount + 0.5) * oneTick;
+	auto counter = 0;
+	const auto tickOnce = [&counter]() { ++counter; };
+	Timer timer;
+	timer.callWithPeriod(tickOnce, oneTick);
+	sleep_for(fullTime);
+	timer.stop();
+	sleep_for(expectedCallCount * oneTick);
+	EXPECT_EQ(counter, expectedCallCount);
+}
+
 // TODO: test the one use exception
 // TODO: test hasStarted
